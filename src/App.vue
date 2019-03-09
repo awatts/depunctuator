b5e2366c16bee39965bdf04c8aee9aa1c6d5d9ae
Andrew Watts <awatts2@ur.rochester.edu>
2019-03-09 13:32:06 -0500

    Add my component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Depunctuator/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Depunctuator from './components/Depunctuator.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Depunctuator
   }
 }
 </script>
